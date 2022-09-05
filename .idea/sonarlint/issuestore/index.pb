d0daae600ca47effeadc21e28398f9e9aa0ce11d
Wladislaw Wellmann <wladislawwellmann@gmail.com>
2022-09-05 11:54:14 +0200

    implemented a regex for the Left Side of the equation

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+C
+hubble-constant.txt,a\e\aee9f7bba4539c8891157176a025ec10745bea1a
--- src/main/java/calc/Application.java
+++ src/main/java/calc/Application.java
@@ -9,53 +9,59 @@ import java.util.Scanner;
 
 
 public class Application {
+    static final String LEFTREGEX = "^([a-z]|[A-Z]|_)([a-z]|[A-Z]|\\d|_)*";
 
-    public static void main(String[] args) {
+    public static void main (String[] args) {
 
-        HashMap<String, Double> hmap = new HashMap<>();
-        Scanner sc= new Scanner(System.in);
+        HashMap<String, Double> hmap = new HashMap<> ();
+        Scanner sc = new Scanner (System.in);
 
         while (true) {
-            System.out.println("Insert your instructions (type \"exit\" for termination):");
-            String exp = sc.nextLine().toLowerCase().trim();
-            if (exp.equals("exit")) {
-                break;
-            }
-
-            if (exp.contains("=")) {
-                String[] split = exp.split("=");
-                Double d = expBuild(split[1]);
-                if (Character.isLetter(split[0].charAt(0))){
-
-                    System.out.println(d==null ? "" : d);
-                    hmap.put(split[0], d);
-                }
-                else{
-                    System.out.println("Error!!!\nHint: Variables have to be on the left side of the term and have to start with a letter.");
-                }
-
-
-            } else {
-                System.out.println(expBuild(hmap, exp));
-            }
+            System.out.println ("Insert your instructions (type \"exit\" for termination):");
+            System.out.print (">");
+            String exp = sc.nextLine ().toLowerCase ().trim ();
+            if (extracted (hmap, exp)) break;
         }
     }
 
-    public static Double expBuild(HashMap<String, Double> hmsd, String s) {
+    private static boolean extracted (HashMap<String, Double> hmap, String exp) {
+
+        if (exp.equals ("exit")) {
+            return true;
+        }
+        if (exp.contains ("=")) {
+            String[] split = exp.split ("=");
+            Double d = expBuild (split[1]);
+
+            if (split[0].matches (LEFTREGEX)) {
+                System.out.println (d == null ? "" : d);
+                hmap.put (split[0], d);
+            } else {
+                System.out.println ("Error!!!\nHint: Variables have to be on the left side of the term and have to start with a letter or a underscrore.");
+            }
+
+
+        } else {
+            System.out.println (expBuild (hmap, exp));
+        }
+        return false;
+    }
+
+    public static Double expBuild (HashMap<String, Double> hmsd, String s) {
         Double ret = null;
         try {
-            Expression eb = new ExpressionBuilder(s).variables(hmsd.keySet()).build();
-            ret = eb.setVariables(hmsd).evaluate();
+            Expression eb = new ExpressionBuilder (s).variables (hmsd.keySet ()).build ();
+            ret = eb.setVariables (hmsd).evaluate ();
 
         } catch (UnknownFunctionOrVariableException e) {
-            System.out.println("Error: First you have to assign variables before computing");
+            System.out.println ("Error: First you have to assign variables before computing");
         }
 
         return ret;
     }
 
-    public static Double expBuild(String s) {
-        return new ExpressionBuilder(s).build().evaluate();
+    public static Double expBuild (String s) {
+        return new ExpressionBuilder (s).build ().evaluate ();
     }
 
 
